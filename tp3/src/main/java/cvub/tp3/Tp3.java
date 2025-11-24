1c02a6b54c457db1bb90a9dc7a35b25691b93c41
utaBdN <utaabn@gmail.com>
2025-11-24 16:52:50 +0100

    first commit

---
+++ tp3/src/main/java/cvub/tp3/Tp3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cvub.tp3;
+
+/**
+ *
+ * @author utaab
+ */
+public class Tp3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
